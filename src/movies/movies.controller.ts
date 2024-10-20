1951d77df7f36dc52b0efc0bcaaa0b6350c10626
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2024-10-20 16:01:23 -0400

    add type on the different layers

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,40 @@
 import { Controller, Get, HttpException, Logger, Param, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { PaginatedMovie } from './types/movies.type';
+import { ListResponse, Movie, Categorie } from './types/movies.type';
 
 @Controller('movies')
 export class MoviesController {
   constructor(
     private readonly moviesService: MoviesService,
-    private readonly logger: Logger,
-  ) {
+    private readonly logger: Logger  ) {
+  }
+
+  @Get('categories')
+  async getCategories(): Promise<Categorie[]> {
+    try {
+      return this.moviesService.getCategories();
+    } catch (error) {
+      this.logger.error('test:', { error });
+      throw new HttpException(error.response.statusText, error.response.status);
+    }
   }
 
   @Get('popular')
-  async getMovies(@Query('page') page: number = 1): Promise<PaginatedMovie> {
+  async getPopularMovies(@Query('page') page: number = 1): Promise<ListResponse<Movie>> {
     try {
       return this.moviesService.getPopularMoviesByPage(page);
     } catch (error) {
-      this.logger.error({error});
+      this.logger.error({ error });
       throw new HttpException(error.response.statusText, error.response.status);
     }
   }
 
   @Get(':id')
-  async getMovieDetails(@Param('id') id: number): Promise<any> {
+  async getMovieDetails(@Param('id') id: number): Promise<Movie> {
     try {
       return this.moviesService.getMovieDetailsById(id);
     } catch (error) {
-      this.logger.error({error});
+      this.logger.error({ error });
       throw new HttpException(error.response.statusText, error.response.status);
     }
   }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { MoviesRepository } from "./movies.repository";
-import { Movie } from "./types/movies.type";
+import { Movie, Categorie, ListResponse } from "./types/movies.type";
 
 @Injectable()
 export class MoviesService {
@@ -8,11 +8,15 @@ export class MoviesService {
   constructor(private readonly moviesRepository: MoviesRepository) {
   }
 
-  async getPopularMoviesByPage(page: number) {
+  async getPopularMoviesByPage(page: number): Promise<ListResponse<Movie>> {
     return this.moviesRepository.getPopularMoviesByPage(page);
   }
 
   async getMovieDetailsById(id: number): Promise<Movie> {
     return this.moviesRepository.getMovieDetailsById(id);
+  }
+
+  async getCategories(): Promise<Categorie[]> {
+    return this.moviesRepository.getCategories();
   }
 }
